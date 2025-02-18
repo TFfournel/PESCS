d2d771dbe81384a106c0d0ae74537ef5f5d50285
tfournel <111280651+Naelig@users.noreply.github.com>
2025-02-18 22:39:41 +0100

    [UPDATE] PathFinding/add DebugLine to see Path

--- Assets/Scripts/Gameplay/Bots/Bot.cs
+++ Assets/Scripts/Gameplay/Bots/Bot.cs
@@ -5,15 +5,43 @@ using UnityEngine.AI;
 
 public class Bot : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] private float _MaxMovementSpeed;
+    [SerializeField] private float _MaxAngularSpeed;
+    [SerializeField] private float _MaxAccelerationSpeed;
+    [SerializeField] private float _StoppingDistance;
+
+
     [Header("PathFinding")]
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Transform target;
     [SerializeField] private bool UseTarget = false;
 
 
-    void Update()
+    public Bot()
+    {
+
+    }
+
+    private void Start()
+    {
+        SetNavMeshAgent();
+    }
+
+    private void SetNavMeshAgent()
+    {
+        agent.speed = _MaxMovementSpeed * Random.Range(0.8f, 1.2f);
+        agent.angularSpeed = _MaxAngularSpeed;
+        agent.acceleration = _MaxAccelerationSpeed;
+        agent.stoppingDistance = _StoppingDistance;
+    }
+
+    private void Update()
     {
         PathFinding();
+        DebugPathRay();
+
+
     }
 
     private void PathFinding()
@@ -29,6 +57,20 @@ public class Bot : MonoBehaviour
             {
                 agent.SetDestination(hit.point);
             }
+        }
+    }
+    private void DebugPathRay()
+    {
+        Vector3[] lPath = agent.path.corners;
+        if (lPath == null || lPath.Length < 2)
+        {
+            return;
+        }
+
+        // Draw lines between consecutive points
+        for (int i = 0; i < lPath.Length - 1; i++)
+        {
+            Debug.DrawLine(lPath[i], lPath[i + 1], Color.green);
         }
     }
 
