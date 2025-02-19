45f4a3a78467d3dbecd34f89814948546552aad8
Sven <sven.bonnavaud@gmail.com>
2025-02-19 11:48:54 +0100

    improve collisionExtensions + listExtensions

--- Assets/Scripts/Gameplay/AI/AiState.cs
+++ Assets/Scripts/Gameplay/AI/AiState.cs
@@ -8,25 +8,38 @@ public class AiStateParameter
 {
 }
 
+public class ChangeStateEventParam: EventArgs
+{
+}
+
 public class AiState
 {
     public float changeToStateFactor;
     public Action behaviourAction = null;
     public Action ChangeToStateFactorCompute = null;
     public AiStateManager aiStateManager;
+    public bool stateActive = false;
+    public EventHandler ChangeToState;
 
     protected virtual void Init(AiStateParameter pParam)
     {
+        ChangeToState += SetChangeState;
     }
 
     protected virtual void Factor()
     {
         if(ChangeToStateFactorCompute != null)
             ChangeToStateFactorCompute();
+        if(changeToStateFactor == 1)
+        {
+            stateActive = true;
+            ChangeToState?.Invoke(this,new EventArgs());
+        }
     }
 
-    protected virtual void SetChangeState()
+    protected virtual void SetChangeState(object pSender,EventArgs pArgs)
     {
+        stateActive = true;
     }
 
     protected virtual float StateChangeCompute()
